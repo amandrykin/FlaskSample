bdde2c9e4ab706ecf6f1d4e73faec1fd2c13f492
amandrykin <Halberd@mail.ru>
2018-09-01 19:28:43 +0300

    Add files via upload

---
+++ microblog.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+
+from app import app, db
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
